958b884e93912875db9a3b1e18bfcf050daaf948
Leet <Leet@DESKTOP-KFDISGM>
2025-02-15 01:35:35 +0300

    comment

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
+//const express = require('express')
+//const app = express()
+//const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+//app.get('/', (req, res) => {
+//	res.send('Hello World!')
+//})
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+//app.listen(port, () => {
+//	console.log(`Example app listening on port ${port}`)
+//})
 
 ////////
 
